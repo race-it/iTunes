aeb1827f529002872758f9a93188eef56c160c96
John Jones <john@race.it>
2013-10-02 11:00:51 -0400

    Initial commit for a receipt processing class.

---
+++ inc/iTunesReceiptValidator.class.php
@@ -0,0 +1,98 @@
+<?php
+
+class iTunesReceiptValidator {
+  private $_iTunesProductionVerifyURL = 'https://buy.itunes.apple.com/verifyReceipt';
+  private $_iTunesSandboxVerifyURL = 'https://sandbox.itunes.apple.com/verifyReceipt';
+  private $_iTunesRetrySandbox = TRUE;
+  private $_endpoint;
+  private $_verbose = FALSE;
+
+  const SANDBOX_RECEIPT_SENT_TO_PRODUCTION_ERROR = 21007;
+
+  function __construct($endpoint, $verbose = FALSE) {
+    $this->setEndPoint($endpoint);
+    $this->setVerbose($verbose);
+  }
+
+  public function setProductionVerifyURL($value) {
+    $this->_iTunesProductionVerifyURL = $value;
+  }
+
+  public function getProductionVerifyURL() {
+    return $this->_iTunesProductionVerifyURL;
+  }
+
+  public function setSandboxVerifyURL($value) {
+    $this->_iTunesSandboxVerifyURL = $value;
+  }
+
+  public function getSandboxVerifyURL() {
+    return $this->_iTunesSandboxVerifyURL;
+  }
+
+  public function setEndpoint($value) {
+    $this->_endpoint = $value;
+  }
+
+  public function getEndpoint() {
+    return $this->_endpoint;
+  }
+
+  public function setVerbose($value) {
+    $this->_verbose = $value;
+  }
+
+  public function getVerbose() {
+    return $this->_verbose;
+  }
+
+  public function validateReceipt($receipt) {
+    $ch = curl_init($this->getEndpoint());
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+
+    $receiptData = (object) array(
+      'receipt-data' => $receipt,
+    );
+
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($receiptData));
+
+    if ($this->getVerbose()) {
+      curl_setopt($ch, CURLOPT_VERBOSE, true);
+    }
+ 
+    // Execute the cURL request and fetch response data.
+    $response = curl_exec($ch);
+
+    $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    curl_close($ch);
+
+    // Ensure the http status code was 200.
+    if ($http_status != 200) {
+      return (object) array(
+        'status' => $http_status
+      );
+    }
+
+    // Parse the response data.
+    $data = json_decode($response);
+
+    // Ensure response data was a valid JSON string.
+    if (!is_object($data)) {
+      return (object) array(
+        'status' => 60001
+      );
+    } 
+
+    if ($data->status === self::SANDBOX_RECEIPT_SENT_TO_PRODUCTION_ERROR && $this->_iTunesRetrySandbox) {
+      $this->setEndpoint($this->getSandboxVerifyURL());
+      return $this->validateReceipt($receipt);
+    }
+
+    return $data;
+
+  }
+}
+?>
